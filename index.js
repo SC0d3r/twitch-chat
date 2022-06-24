52370d10cc149e0f30c296620e87545912d2f2e2
SC0d3r <sasan.kelishani@gmail.com>
2022-06-24 05:04:41 +0430

    bold font for username and more colors

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ parent.addEventListener("DOMNodeInserted",(e) => {
 
     //replaceAllImagesWithAlt(p)
     const message = p.querySelector(messageTextDataAttr).innerText.trim()
-    console.log("[" + `%c${username}` + "%c] -> " + message,`color: ${rndColor()};`, "color: #000000;")
+    console.log("[" + `%c${username}` + "%c] -> " + message,`color: ${rndColor()}; font-weight: bold;`, "color: #000000;")
   }
 })
 
 parent.addEventListener("DOMNodeRemoved",(e) => {
   if(e.target.getAttribute("data-test-selector") === "chat-line-message-body"){
-  //if(e.target.getAttribute("data-a-target") === "chat-message-text"){
     const messageEl = e.target
     const commonParent = messageEl.parentElement
     const username =commonParent.querySelector("[data-a-target=chat-message-username]").innerText
@@ -29,21 +28,12 @@ parent.addEventListener("DOMNodeRemoved",(e) => {
 })
 
 function rndColor(){
-  return "#" + Math.round(Math.random() * 16777215).toString(16)
+  const colors = ["#c0392b","#8e44ad","#d63031", "#2d3436" , "#130f40", "#4834d4",
+  "#009432", "#1B1464", "#6F1E51", "#EE5A24", "#ffa801"]
+  const rndIndex = Math.floor(Math.random() * colors.length)
+  //return "#" + Math.round(Math.random() * 16777215).toString(16)
+  return colors[rndIndex]
 }
-
-function replaceAllImagesWithAlt(parent){
-  const allNodes= parent.querySelectorAll("img")
-  const all  = [...allNodes]
-  all.forEach(el => {
-    const alt = el.alt
-    const newChild = document.createElement("div")
-    newChild.innerText = alt
-    parent.replaceChild(newChild, el)
-  })
-}
-  
-
 
 
 /*
