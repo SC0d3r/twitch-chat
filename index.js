b9190f69f9ff797d71ab1ef1388521a025c17c4f
SC0d3r <sasan.kelishani@gmail.com>
2022-06-24 06:14:10 +0430

    add config and sound

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
+// config variables
+let _config = {
+  allMessages: 1, // 1 means show all the chat messages
+  deletedMessages: 1,
+  sound: 0,
+  clear: 1
+}
+
+// this try checks if user has allready defined his own
+// config
+try{
+  // so user can use his own config
+  // before running this program in console
+  _config = {..._config, ...config}
+}catch(e){}
+
+ const beep = new Audio(
+'https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+beep.volume = 0.1;
+
 const data_test_selector="chat-scrollable-area__message-container"
 const messageTextDataAttr = "[data-test-selector=chat-line-message-body]"
 
 const parent = document.querySelector("[data-test-selector=chat-scrollable-area__message-container]")
 const withoutMentionMessageTextDataAttr = "[data-a-target=chat-message-text]"
 
-parent.addEventListener("DOMNodeInserted",(e) => {
-  const p = e.target
-  if(p.getAttribute("data-a-target") === "chat-line-message"){
-    const username = p.querySelector("[data-a-target=chat-message-username]").innerText
+if(_config.allMessages)
+  parent.addEventListener("DOMNodeInserted",(e) => {
+    const p = e.target
+    if(p.getAttribute("data-a-target") === "chat-line-message"){
+      const username = p.querySelector("[data-a-target=chat-message-username]").innerText
 
-    //replaceAllImagesWithAlt(p)
-    const message = p.querySelector(messageTextDataAttr).innerText.trim()
-    console.log("[" + `%c${username}` + "%c] -> " + message,`color: ${rndColor()}; font-weight: bold;`, "color: #000000;")
-  }
-})
+      //replaceAllImagesWithAlt(p)
+      const message = p.querySelector(messageTextDataAttr).innerText.trim()
+      console.log("[" + `%c${username}` + "%c] -> " + message,`color: ${rndColor()}; font-weight: bold;`, "color: #000000;")
+    }
+  })
 
-parent.addEventListener("DOMNodeRemoved",(e) => {
-  if(e.target.getAttribute("data-test-selector") === "chat-line-message-body"){
-    const messageEl = e.target
-    const commonParent = messageEl.parentElement
-    const username =commonParent.querySelector("[data-a-target=chat-message-username]").innerText
-    const message = messageEl.innerText.trim()
+if(_config.deletedMessages)
+  parent.addEventListener("DOMNodeRemoved",(e) => {
+    if(e.target.getAttribute("data-test-selector") === "chat-line-message-body"){
+      if(_config.sound)
+        beep.play()
 
-    console.log(`%c ::DELETED:: [${username}] -> ${message}`, 'background: #222; color: #bada55');
-  
-  }
-})
+      const messageEl = e.target
+      const commonParent = messageEl.parentElement
+      const username =commonParent.querySelector("[data-a-target=chat-message-username]").innerText
+      const message = messageEl.innerText.trim()
+
+      console.log(`%c ::DELETED:: [${username}] -> ${message}`, 'background: #222; color: #bada55');
+    
+    }
+  })
 
 function rndColor(){
   const colors = ["#c0392b","#8e44ad","#d63031", "#2d3436" , "#130f40", "#4834d4",
@@ -35,7 +60,7 @@ function rndColor(){
   return colors[rndIndex]
 }
 
-
+if(_config.clear) console.clear()
 /*
 # the main parent of each chat message
 data-a-target=chat-line-message
